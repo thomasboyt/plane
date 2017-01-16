053dae0342c7bca1bc41949532a98de35f39a0ab
Thomas Boyt <me@thomasboyt.com>
2017-01-16 18:39:08 -0500

    Add collision

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import * as SAT from 'sat';
+
 import {registerListeners, keysDown} from './util/inputter';
 import keyCodes from './util/keyCodes';
 import {degreesToRadians, calcVectorRadians} from './util/math';
@@ -5,6 +7,8 @@ import {degreesToRadians, calcVectorRadians} from './util/math';
 const WIDTH = 320;
 const HEIGHT = 240;
 const PLANE_SIDE_LENGTH = 20;
+const CAMERA_OFFSET = 80;
+const WALL_WIDTH = 20;
 
 const ABS_MAX_ANGLE_DEG = 10;
 const MIN_ANGLE = degreesToRadians(ABS_MAX_ANGLE_DEG);
@@ -27,23 +31,49 @@ interface Plane {
 }
 
 class State {
-  blocks: Block[] = [];
+  dead: boolean;
+
+  blocks: Block[];
   plane: Plane;
 
+  points: number;
+
   constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.dead = false;
+
+    this.blocks = [
+      {x: 0, y: 100, width: 160, height: 40},
+      {x: WIDTH - 160, y: 200, width: 160, height: 40},
+      {x: 0, y: 300, width: 160, height: 40},
+    ];
+
     this.plane = {
-      x: 20,
+      x: 40,
       y: 20,
       speed: 100,
       angle: MIN_ANGLE,
     };
 
-    this.blocks = [
-      {x: 0, y: 100, width: 160, height: 40}
-    ];
+    this.points = 0;
   }
 
   update(dt: number, keysDown: Set<number>) {
+    if (this.dead) {
+      if (keysDown.has(keyCodes.SPACE)) {
+        this.reset();
+      }
+
+      return;
+    }
+
+    /*
+     * Handle input
+     */
+
     if (keysDown.has(keyCodes.A)) {
       // turn left
       this.plane.angle -= ROTATION_SPEED * dt;
@@ -62,13 +92,68 @@ class State {
       this.plane.angle = MIN_ANGLE;
     }
 
+    /*
+     * Update plane position
+     */
+
     const {x, y} = calcVectorRadians(this.plane.speed * dt, this.plane.angle);
     this.plane.x += x;
     this.plane.y += y;
 
-    // TODO: Clean up platforms no longer on screen
+    /*
+     * Test collisions
+     */
 
-    // TODO: Spawn more platforms
+    const planeCollider = new SAT.Polygon(new SAT.Vector(this.plane.x, this.plane.y), [
+      new SAT.Vector(-PLANE_SIDE_LENGTH / 2, -PLANE_SIDE_LENGTH / 2),
+      new SAT.Vector(-PLANE_SIDE_LENGTH / 2, PLANE_SIDE_LENGTH / 2),
+      new SAT.Vector(PLANE_SIDE_LENGTH / 2, 0),
+    ]);
+    planeCollider.rotate(this.plane.angle);
+
+    const blockColliders = this.blocks.map((block) => {
+      return new SAT.Box(new SAT.Vector(block.x, block.y), block.width, block.height).toPolygon();
+    });
+
+    let resp: SAT.Response;
+    let collided: boolean = false;
+    for (let blockCollider of blockColliders) {
+      resp = new SAT.Response();
+      collided = SAT.testPolygonPolygon(planeCollider, blockCollider);
+      if (collided) {
+        break;
+      }
+    }
+
+    if (!collided) {
+      // check collision with walls
+      for (let point of planeCollider.calcPoints) {
+        const x = point.x + planeCollider.pos.x;
+        if (x < WALL_WIDTH || x > WIDTH - WALL_WIDTH) {
+          collided = true;
+        }
+      }
+    }
+
+    if (collided) {
+      this.dead = true;
+      return;
+    }
+
+    /*
+     * Update blocks
+     */
+
+    // Clean up offscreen blocks
+    this.blocks = this.blocks.filter((block) => {
+      const cameraTop = this.plane.y - CAMERA_OFFSET;
+      const blockBottom = block.y + block.height;
+      return cameraTop <= blockBottom;
+    });
+
+    // TODO: Spawn more blocks using some logic!!
+
+    // TODO: Increment points when player passes a block
   }
 }
 
@@ -104,7 +189,7 @@ function draw(state: State, canvas: HTMLCanvasElement) {
 
   // Draw platforms + plane (which scroll)
   ctx.save();
-  ctx.translate(0, -(state.plane.y - 80));
+  ctx.translate(0, -(state.plane.y - CAMERA_OFFSET));
 
   for (let block of state.blocks) {
     ctx.fillRect(block.x, block.y, block.width, block.height);
@@ -115,10 +200,10 @@ function draw(state: State, canvas: HTMLCanvasElement) {
   ctx.restore();
 
   // Draw walls
-  ctx.fillRect(0, 0, 20, HEIGHT);
-  ctx.fillRect(WIDTH - 20, 0, 20, HEIGHT);
+  ctx.fillRect(0, 0, WALL_WIDTH, HEIGHT);
+  ctx.fillRect(WIDTH - WALL_WIDTH, 0, WALL_WIDTH, HEIGHT);
 
-  // TODO: un-translate for HUD
+  // TODO: Draw HUD
 }
 
 let then = Date.now();
@@ -136,8 +221,7 @@ function runLoop(state: State, canvas: HTMLCanvasElement) {
 }
 
 function main() {
-  const canvas = document.createElement('canvas');
-  document.body.appendChild(canvas);
+  const canvas = document.querySelector('canvas')!;
   registerListeners();
 
   canvas.width = WIDTH;
